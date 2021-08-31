6ba13e895eb08bf4c5bdcc997e30f02d57527eba
38240 <76523545+38240@users.noreply.github.com>
2021-08-31 18:25:02 +0800

    完成轮播图

--- RotationChart/js/index.js
+++ RotationChart/js/index.js
@@ -7,36 +7,79 @@ window.addEventListener("load", function () {
 
     for (var i = 0; i < ul.children.length; i++) {
         var li = document.createElement('li')
+        li.setAttribute('index', i)
         ol.appendChild(li)
+        li.addEventListener('click', function () {
+            for (var i = 0; i < ol.children.length; i++) {
+                ol.children[i].id = ''
+            }
+            this.id = 'current'
+            var index = this.getAttribute('index')
+            num = index
+            circle = index
+            animate(ul, -index * main.offsetWidth)
+        })
     }
     ol.children[0].id = 'current'
 
     main.addEventListener('mouseenter', function () {
         arrow_l.style.display = 'block'
         arrow_r.style.display = 'block'
+        clearInterval(timer)
     })
     main.addEventListener('mouseleave', function () {
         arrow_l.style.display = 'none'
         arrow_r.style.display = 'none'
+        timer = setInterval(function (){
+            arrow_r.click()
+        },2000)
     })
 
     var first = ul.children[0].cloneNode(true)
     ul.appendChild(first)
     var num = 0
+    var circle = 0
+    var flag = true
     arrow_r.addEventListener('click', function () {
-        if (num == ul.children.length - 1){
-            ul.style.left = 0
-            num = 0
+        if (flag){
+            flag = false
+            if (num == ul.children.length - 1) {
+                ul.style.left = 0
+                num = 0
+            }
+            num++
+            animate(ul, -num * main.offsetWidth,function (){flag = true})
+            circle++
+            if (circle == ol.children.length) {
+                circle = 0
+            }
+            Change()
         }
-        num++
-        animate(ul, -num * main.offsetWidth)
     })
     arrow_l.addEventListener('click', function () {
-        if (num == 0){
-            num = ul.children.length - 1
-            ul.style.left = -num * main.offsetWidth + 'px'
+        if (flag){
+            if (num == 0) {
+                num = ul.children.length - 1
+                ul.style.left = -num * main.offsetWidth + 'px'
+            }
+            num--
+            animate(ul, -num * main.offsetWidth,function (){flag = true})
+            circle--
+            if (circle < 0) {
+                circle = ol.children.length
+            }
+            Change()
         }
-        num--
-        animate(ul, -num * main.offsetWidth)
     })
+
+    function Change() {
+        for (var i = 0; i < ol.children.length; i++) {
+            ol.children[i].id = ''
+        }
+        ol.children[circle].id = 'current'
+    }
+
+    var timer = setInterval(function (){
+        arrow_r.click()
+    },2000)
 })
